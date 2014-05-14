7e2fc6cf1ecfa96a78dd1933fc95b28444467a44
Shane A. Stillwell <shanestillwell@gmail.com>
2014-05-14 18:16:40 -0500

    Update cron.rb

--- mongodb-mysite/recipes/cron.rb
+++ mongodb-mysite/recipes/cron.rb
@@ -11,3 +11,16 @@ template 'mongobackup.sh' do
   group 'root'
   mode 0644
 end
+
+template 's3cfg.sh' do
+  case node[:platform]
+  when 'centos','redhat','fedora','amazon'
+    path "/root/.s3cfg"
+  when 'debian','ubuntu'
+    path "/root/.s3cfg"
+  end
+  source 's3cfg.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
